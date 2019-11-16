40b26dc27a426d47b2cde1349e0630bcd122b90c
Bruno Luiz Silva <contact@brunoluiz.net>
2019-11-16 23:50:17 +0000

    chore: update manifest and other stuff

--- scripts/action.js
+++ scripts/action.js
@@ -20,7 +20,7 @@ const setMessage = data =>
 document.addEventListener("DOMContentLoaded", function() {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
-      if (!response.success) {
+      if (!response || (response && !response.success)) {
         setMessage({
           msg: "🚨 Error on markdown parsing",
           type: MESSAGE_ERROR
