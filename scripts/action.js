6c35bb9381d703a390b3a0b959a7779b0aaaad34
Bruno Luiz Silva <contact@brunoluiz.net>
2020-05-03 10:13:44 +0100

    chore: move parsing from content to action

--- scripts/action.js
+++ scripts/action.js
@@ -1,6 +1,13 @@
 const MESSAGE_SUCCESS = "success";
 const MESSAGE_ERROR = "error";
 
+const turndownService = new TurndownService({
+  headingStyle: "atx",
+  linkStyle: "referenced",
+  bulletListMarker: "-",
+  br: ""
+});
+
 const copyToClipboard = str => {
   const el = document.createElement("textarea");
   el.value = str;
@@ -19,17 +26,25 @@ const setMessage = data =>
 
 document.addEventListener("DOMContentLoaded", function() {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
-      if (!response || (response && !response.success)) {
-        setMessage({
+    chrome.tabs.sendMessage(tabs[0].id, {}, function(response = {}) {
+      const { content } = response;
+      if (!content) {
+        return setMessage({
+          msg: "🚨 Error on getting content",
+          type: MESSAGE_ERROR
+        });
+      }
+
+      try {
+        const markdown = turndownService.turndown(content);
+        copyToClipboard(markdown);
+        setMessage({ msg: "📋 Copied to clipboard", type: MESSAGE_SUCCESS });
+      } catch (err) {
+        return setMessage({
           msg: "🚨 Error on markdown parsing",
           type: MESSAGE_ERROR
         });
-        return;
       }
-
-      copyToClipboard(response.markdown);
-      setMessage({ msg: "📋 Copied to clipboard", type: MESSAGE_SUCCESS });
     });
   });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,6 @@
-const turndownService = new TurndownService({
-  headingStyle: "atx",
-  linkStyle: "referenced",
-  bulletListMarker: "-",
-  br: ""
-});
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  try {
-    const markdown = turndownService.turndown(
-      document.querySelector(".ql-editor").innerHTML
-    );
-    sendResponse({ markdown, success: true });
-  } catch (err) {
-    sendResponse({ success: false });
-  }
+  console.log("oi");
+  sendResponse({
+    content: document.querySelector(".ql-editor").innerHTML
+  });
 });
