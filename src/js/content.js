d5f555dd6b06e37a198c1ea8bbae400dfd69d57c
Bruno Luiz Silva <bsilva@utilitywarehouse.co.uk>
2020-07-17 11:50:29 +0100

    feat: add title as h1 and transform content h{1,2} to h{2,3} (#8)

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   sendResponse({
+    title: document.querySelector("input[data-name=title-input]").value,
     content: document.querySelector(".ql-editor").innerHTML,
   });
 });
