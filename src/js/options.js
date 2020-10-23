01b7fcbfb3a6b77177679fa57bdaf70f0bb76492
Gianluca Arbezzano <gianarb92@gmail.com>
2020-10-23 09:48:34 +0200

    Fix linting issue

--- src/js/options.js
+++ src/js/options.js
@@ -1,18 +1,20 @@
 // Saves options to chrome.storage
-function save_options() {
-  var linkStyle = document.getElementById('turndown-linkSyle').value;
+const saveOptions = () => {
+  const linkStyle = document.getElementById("turndown-linkSyle").value;
   chrome.storage.sync.set({
-    linkStyle: linkStyle
+    linkStyle: linkStyle,
   });
-}
+};
 
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    linkStyle: "referenced"
-  }, function(items) {
-    document.getElementById('turndown-linkSyle').value = items.linkStyle;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+const restoreOptions = () => {
+  chrome.storage.sync.get(
+    {
+      linkStyle: "referenced",
+    },
+    (items) => {
+      document.getElementById("turndown-linkSyle").value = items.linkStyle;
+    }
+  );
+};
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
