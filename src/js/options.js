683a80e4dc01442c38f1ae1b7938b92603904dba
Bruno Luiz Silva <contact@brunoluiz.net>
2020-11-09 21:32:11 +0000

    feat: add more custom options

--- src/js/options.js
+++ src/js/options.js
@@ -1,20 +1,46 @@
+import "../css/options.css";
+
 // Saves options to chrome.storage
 const saveOptions = () => {
-  const linkStyle = document.getElementById("turndown-linkSyle").value;
   chrome.storage.sync.set({
-    linkStyle: linkStyle,
+    turndown: {
+      linkStyle: document.getElementById("turndown-linkSyle").value,
+      bulletListMarker: document.getElementById("turndown-bulletListMarker")
+        .value,
+      emDelimiter: document.getElementById("turndown-emDelimiter").value,
+      strongDelimiter: document.getElementById("turndown-strongDelimiter")
+        .value,
+    },
+    escapeBackticks: !!document.getElementById("turndown-escapeBackticks")
+      .checked,
   });
 };
 
 const restoreOptions = () => {
   chrome.storage.sync.get(
     {
-      linkStyle: "referenced",
+      turndown: {
+        bulletListMarker: "-",
+        listStyle: "referenced",
+        strongDelimiter: "**",
+        emDelimiter: "_",
+      },
+      escapeBackticks: true,
     },
     (items) => {
-      document.getElementById("turndown-linkSyle").value = items.linkStyle;
+      document.getElementById("turndown-linkSyle").value =
+        items.turndown.linkStyle;
+      document.getElementById("turndown-bulletListMarker").value =
+        items.turndown.bulletListMarker;
+      document.getElementById("turndown-emDelimiter").value =
+        items.turndown.emDelimiter;
+      document.getElementById("turndown-strongDelimiter").value =
+        items.turndown.strongDelimiter;
+      document.getElementById("turndown-escapeBackticks").checked =
+        items.escapeBackticks;
     }
   );
 };
+
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.getElementById("save").addEventListener("click", saveOptions);
--- src/js/popup.js
+++ src/js/popup.js
@@ -23,19 +23,16 @@ const setMessage = (data) =>
     el.classList.add(data.type);
   }, 500);
 
-document.addEventListener("DOMContentLoaded", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {}, function (response = {}) {
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {}, (response = {}) => {
       chrome.storage.sync.get(
         {
-          headingStyle: "atx",
-          linkStyle: "referenced",
-          bulletListMarker: "-",
-          br: "",
+          turndown: {},
+          escapeBackticks: true,
         },
-        function (items) {
-          console.log(items);
-          var turndownService = new TurndownService(items);
+        ({ turndown, escapeBackticks }) => {
+          const turndownService = new TurndownService(turndown);
           turndownService.addRule("h1", {
             filter: ["h1"],
             replacement: (content) => `## ${content}\n`,
@@ -58,6 +55,10 @@ document.addEventListener("DOMContentLoaded", function () {
             let markdown = "";
             markdown += `# ${title}\n\n`;
             markdown += turndownService.turndown(content);
+            if (!escapeBackticks) {
+              markdown = markdown.replaceAll("\\`", "`");
+            }
+
             copyToClipboard(markdown);
             setMessage({
               msg: "📋 Copied to clipboard",
