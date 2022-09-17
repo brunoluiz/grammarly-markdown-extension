5875abc4a4f81449ff4c54306400b5a6f95370b1
Sina Atalay <sinatalay@hotmail.com>
2022-09-17 19:21:14 +0300

    feat: add 'Escape markdown characters?' option 2

--- src/js/globals.js
+++ src/js/globals.js
@@ -5,6 +5,7 @@ window.G2M_DEFAULT_SETTINGS = Object.freeze({
     listStyle: "referenced",
     strongDelimiter: "**",
     emDelimiter: "_",
+    escapeMarkdown: true,
   },
   escapeBackticks: false,
 });
